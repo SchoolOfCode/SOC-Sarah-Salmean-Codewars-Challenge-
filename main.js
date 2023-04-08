f8cf4a4989c02091bf038c732f6dbba9f9b0c8cd
Sarah Salmean <sarahsalmean@hotmail.com>
2023-04-08 16:32:24 +0200

    Codewars task details

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 /* 
-👉 Write your kata here!
+The cure for insomnia 
+
+You have been having trouble sleeping. A friend reccomends you try watching the experimental film "The Cure for Insomnia" - the worlds longest film, with a run time of 87 hours. 
+
+You somehow obtain a copy despite the film being considered 'lost'.
+
+Write a funtion to calculate how many days pass before you get through the full film, with the following factors in mind:
+
+1) You put the film on when you go to bed each night at 10pm, and watch it until you fall asleep. This is the only timeslot that you watch the film.
+
+2) Regardless of if you slept that night, you always get up at 7am. 
+
+3) You never wake during the night, and the film stops playing the moment you fall asleep. If you do not sleep that night, the film stops at 7am when you get up.
+
+4) Each night, you pick up from the point in the film where you left off.
+
+5) The first night watching the film, you manage to fall asleep after 3 hours.
+
+6) On night 2, you do not sleep at all. Every second night from then on, you do not sleep at all.
+
+7) On the nights that you do sleep, it alternates between taking 3 hours to get to sleep one night, and taking 4 hours to get to sleep the following night.
+
+8) You have five sleeping pills that will grant you instant sleep, which you take on random evenings.
+
 */
 
+
+
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+const filmDays = function() {
+
+return ;
+}
