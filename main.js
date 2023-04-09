8264fd31d4694e5643ac50634da425cea40f84a9
Sarah Salmean <sarahsalmean@hotmail.com>
2023-04-09 13:41:20 +0200

    Fixed typo

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 /* 
-The cure for insomnia 
+The cure for insomnia:
 
-You have been having trouble sleeping. A friend reccomends you try watching the experimental film "Logistics" (The Logistics Art Project: https://en.wikipedia.org/wiki/Logistics_(film) ) - the worlds longest film, with a run time of 857 hours. 
+You have been having trouble sleeping. A friend recommends you try watching the experimental film "Logistics" (The Logistics Art Project: https://en.wikipedia.org/wiki/Logistics_(film) ) - the worlds longest film, with a run time of 857 hours.
 
 You somehow obtain a copy despite the film being considered 'lost'.
 
-Write a funtion to calculate how many days pass before you get through the full film, with the following factors in mind:
+Write a function to calculate how many days pass before you get through the full film, with the following factors in mind:
 
 1) You put the film on when you go to bed each night at 10pm, and watch it until you fall asleep. This is the only timeslot that you watch the film.
 
-2) Regardless of if you slept that night, you always get up at 7am. 
+2) Regardless of if you slept that night, you always get up at 7am.
 
 3) You never wake during the night, and the film stops playing the moment you fall asleep. If you do not sleep that night, the film stops at 7am when you get up.
 
@@ -21,9 +21,9 @@ Write a funtion to calculate how many days pass before you get through the full 
 
 7) On the nights that you do sleep, it alternates between getting 5 hours of sleep one night, and 4 hours of sleep the following night.
 
-8) You have ten sleeping pills that will grant you instant sleep, which you take on random no sleep nights. 
+8) You have ten sleeping pills that will grant you instant sleep, which you take on random no sleep nights.
 
-9) Display the number of nights as a whole interger, rounding upwards.
+9) Display the number of nights as a whole integer, rounding upwards.
 
 */
 
